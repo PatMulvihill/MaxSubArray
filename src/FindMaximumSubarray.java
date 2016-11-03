e55cd26907204bdefd2fdc0c1cec81a06cc067e1
PatMulvihill <mulvihillp993@yahoo.com>
2016-11-02 21:20:30 -0400

    implemented findMaximumSubarray function, have to figure out crossing sum next

--- src/FindMaximumSubarray.java
+++ src/FindMaximumSubarray.java
@@ -6,10 +6,32 @@
  */
 import java.util.*;
 public class FindMaximumSubarray {
-    public static int maxSubarraySum(int []arr, int a, int b) {
-        int maxSum = 0;
+    public static int[] findMaximumSubarray(int[] arr, int low, int high) {
+        int[] sumArr = new int[3];
+        if (high == low) {
+            sumArr[0] = low;
+            sumArr[1] = high;
+            sumArr[2] = arr[low];
+            return sumArr;
+        }
+        else {
+            int mid = (low + high) / 2;
+            int[] leftSumArr = new int[3];
+            leftSumArr = findMaximumSubarray(arr, low, mid);
+            int[] rightSumArr = new int[3];
+            rightSumArr = findMaximumSubarray(arr, mid + 1, high);
+            int[] crossSumArr = new int[3];
+            crossSumArr = findMaxCrossingSubarray(arr, low, mid, high);
+            if (leftSumArr[2] >= rightSumArr[2] && leftSumArr[2] >= crossSumArr[2]) {
+                return leftSumArr;
+            } else if (rightSumArr[2] >= leftSumArr[2] && rightSumArr[2] >= crossSumArr[2]) {
+                return rightSumArr;
+            } else {
+                return crossSumArr;
+            }
+        }
         
-        return maxSum;
+        
     }
     
     
@@ -18,10 +40,10 @@ public class FindMaximumSubarray {
     public static void main(String[] args) {
         int[] dataArr = {15, 12, 19, 3, 9, 10, 11, 12, 5, 7,
                          4, 1, 3, 4, 3, 10, 11, 12, 7, 6};
-        int maxSum = maxSubarraySum(dataArr, 0, dataArr.length - 1);
+        int[] maxSum = findMaximumSubarray(dataArr, 0, dataArr.length - 1);
         System.out.println("data array = " + Arrays.toString(dataArr));
         System.out.println("The Maximum sub array sum for the data array is: "
-                + maxSum);
+                + Arrays.toString(maxSum));
     }
     
 }
